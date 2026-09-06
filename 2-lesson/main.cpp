3e30a2289ef66295b732fe1748cab17cb116635c
k.pimenov <kostalexo31@gmail.com>
2026-09-06 23:26:11 +0300

    feat(2-lesson): second lesson - practice input and output

---
+++ 2-lesson/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {   
+    int age;
+    double weight;
+    std::cout << "Input age and weight: ";
+    std::cin >> age >> weight;
+    std::cout << "Age: " << age << "\t Weight: " << weight << std::endl;
+    return 0;
+}
